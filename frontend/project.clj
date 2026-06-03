e9f8286ce135fb186f4139d05e86dd99a6343735
Clara Andrade <mariaclaraandrade140@gmail.com>
2026-06-03 08:21:12 -0300

    Atualizações na API backend e frontend

--- frontend/project.clj
+++ frontend/project.clj
@@ -1,14 +1,14 @@
 (defproject trabfinal-app "0.1.0-SNAPSHOT"
   :description "Calculadora de Calorias - Frontend"
-  :dependencies [[org.clojure/clojure "1.12.2"]
-                 [ring/ring-core "1.12.2"]
-                 [ring/ring-jetty-adapter "1.12.2"]
-                 [ring/ring-defaults "0.5.0"]
-                 [compojure "1.7.1"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ring/ring-core "1.8.1"]
+                 [ring/ring-jetty-adapter "1.8.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [compojure "1.6.1"]
                  [hiccup "1.0.5"]
-                 [clj-http "3.13.0"]
-                 [cheshire "5.13.0"]]
-  :main ^:skip-aot trabfinal-app.core
+                 [clj-http "3.12.3"]
+                 [cheshire "5.10.0"]]
+  :main trabfinal-app.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
